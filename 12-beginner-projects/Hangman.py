a335efd72eb7d19e215b03f2ed8f6ead19a8fac9
yagmurdenizlii <denizliyagmur01@gmail.com>
2022-07-15 12:17:19 +0200

    hangman

--- 12-beginner-projects/Hangman.py
+++ 12-beginner-projects/Hangman.py
@@ -7,7 +7,7 @@ from words import words
 def get_valid_word(words):
     word = random.choice(words)
     while '-' in word or ' ' in word:
-        word = random.choice(words)
+        word = random.choice(words) 
     return word.upper()
 
 
@@ -19,9 +19,12 @@ def hangman():
     used_letters = set()
     word_letters = set(word)
 
-    while len(word_letters) > 0:
+    lives = 10
+
+    while len(word_letters) > 0 and lives > 0:
 
         print('You have used these letters:', ' '.join(used_letters))
+        print(f"You have {lives} lives left.")
         
         word_list = []
         #loops the word, puts - if that letter has already been used
@@ -32,7 +35,7 @@ def hangman():
                 word_list.append('-')
         print('Current situation:', ' '.join(word_list)) #if we don't use join, there will be ' ' for every letter
 
-        # Better way:
+        # Shorter way:
         # word_list = [letter if letter in used_letters else '-' for letter in word]
         # print("current word:", ' '.join(word_list))
 
@@ -45,8 +48,13 @@ def hangman():
             if guessed_letter in word_letters:
                 print("This letter is in the word")
                 word_letters.remove(guessed_letter)
+            else:
+                lives -= 1
 
-    print(f"You have guessed the word {word} correctly.")
+    if lives == 0:
+        print("You lost.")
+    else:
+        print(f"You have guessed the word {word} correctly.")
 
 
 hangman()
